4a9cbae3e9ccd5cd1f2e766306d329753c48fde1
jarbasal <jarbasai@mailfence.com>
2020-08-03 01:36:10 +0100

    #1

--- __init__.py
+++ __init__.py
@@ -13,8 +13,14 @@ class KittSkinSkill(MycroftSkill):
         # speak animation
         self.add_event('recognizer_loop:audio_output_start',
                        self.handle_speak_start)
+        self.add_event('enclosure.mouth.talk', self.handle_speak_start)
         self.add_event('recognizer_loop:audio_output_end',
-                       self.handle_speak_end)
+                       self.handle_reset)
+
+        # listen
+        self.add_event('recognizer_loop:record_begin', self.handle_listen)
+        self.add_event('enclosure.mouth.listen', self.handle_listen)
+        self.add_event('recognizer_loop:record_end', self.handle_reset)
 
         # clear gui
         self.add_event("mycroft.skill.handler.complete",
@@ -23,43 +29,42 @@ class KittSkinSkill(MycroftSkill):
                        self.handle_reset)
 
         # TODO: Register handlers for standard (Mark 1) events
-        # self.bus.on('enclosure.eyes.on', self.on)
-        # self.bus.on('enclosure.eyes.off', self.off)
-        # self.bus.on('enclosure.eyes.blink', self.blink)
-        # self.bus.on('enclosure.eyes.narrow', self.narrow)
-        # self.bus.on('enclosure.eyes.look', self.look)
-        # self.bus.on('enclosure.eyes.color', self.color)
-        # self.bus.on('enclosure.eyes.level', self.brightness)
-        # self.bus.on('enclosure.eyes.volume', self.volume)
-        # self.bus.on('enclosure.eyes.spin', self.spin)
-        # self.bus.on('enclosure.eyes.timedspin', self.timed_spin)
-        # self.bus.on('enclosure.eyes.reset', self.reset)
-        # self.bus.on('enclosure.eyes.setpixel', self.set_pixel)
-        # self.bus.on('enclosure.eyes.fill', self.fill)
+        # self.add_event('enclosure.eyes.on', self.on)
+        # self.add_event('enclosure.eyes.off', self.off)
+        # self.add_event('enclosure.eyes.blink', self.blink)
+        # self.add_event('enclosure.eyes.narrow', self.narrow)
+        # self.add_event('enclosure.eyes.look', self.look)
+        # self.add_event('enclosure.eyes.color', self.color)
+        # self.add_event('enclosure.eyes.level', self.brightness)
+        # self.add_event('enclosure.eyes.volume', self.volume)
+        # self.add_event('enclosure.eyes.spin', self.spin)
+        # self.add_event('enclosure.eyes.timedspin', self.timed_spin)
+        # self.add_event('enclosure.eyes.reset', self.reset)
+        # self.add_event('enclosure.eyes.setpixel', self.set_pixel)
+        # self.add_event('enclosure.eyes.fill', self.fill)
 
-        # self.bus.on('enclosure.mouth.talk', self.talk)
-        # self.bus.on('enclosure.mouth.listen', self.listen)
-        # self.bus.on('enclosure.mouth.smile', self.smile)
-        # self.bus.on('enclosure.mouth.viseme', self.viseme)
-
-        # self.bus.on('recognizer_loop:record_begin', self.mouth.listen)
-        # self.bus.on('recognizer_loop:record_end', self.mouth.reset)
-        pass
+        # self.add_event('enclosure.mouth.smile', self.smile)
+        # self.add_event('enclosure.mouth.viseme', self.viseme)
 
     @resting_screen_handler("KITT")
     def idle(self, message=None):
-        self.handle_think()
+        # TODO blacklist mark2 skill since it conflicts
+        self.gui.show_animated_image(join(dirname(__file__),
+                                          "ui", "idle.gif"),
+                                     override_idle=True)
 
     def handle_speak_start(self, message=None):
         self.gui.show_animated_image(join(dirname(__file__),
                                           "ui", "speak.gif"),
                                      override_idle=True)
 
-    def handle_speak_end(self, message=None):
-        self.idle()
-
     def handle_reset(self, message=None):
         self.gui.clear()
+
+    def handle_listen(self):
+        self.gui.show_animated_image(join(dirname(__file__),
+                                          "ui", "listening.gif"),
+                                     override_idle=True)
 
     def handle_think(self, message=None):
         self.gui.show_animated_image(join(dirname(__file__),
